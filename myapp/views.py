47055067a8ee5738e9f3481e9c4236149e58d214
arj0011 <arj0011@gmail.com>
2025-06-06 00:16:16 +0530

    remove comment

--- myapp/views.py
+++ myapp/views.py
@@ -49,11 +49,11 @@ def signup(request):
             )
             # new_user.is_active = False
             new_user.save()
-            # send_registration_email.delay(
-            #     to_email=new_user.email,
-            #     subject="Welcome to PlugnPlay",
-            #     message="Thanks for registering with us!"
-            # )
+            send_registration_email.delay(
+                to_email=new_user.email,
+                subject="Welcome to PlugnPlay",
+                message="Thanks for registering with us!"
+            )
         except:
             pass
         return redirect('/signin/')
